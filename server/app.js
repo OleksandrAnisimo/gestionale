548c9b889a2072fc426ae270719febe9b59c4d7c
top-dev617 <acelancer617@gmail.com>
2023-12-11 16:42:54 +0100

    Updated client path

--- server/app.js
+++ server/app.js
@@ -23,10 +23,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
